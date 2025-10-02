6229e65bee26180cb8f45b9340bda41deff45d2a
Axel Irribarren <axelirribarren@gmail.com>
2025-10-02 19:20:33 -0300

    defino estructuras para el grafo

--- practicamia.cpp
+++ practicamia.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
+///------ESTRUCTURA BASICA------///
+//PARA CREAR UNA LISTA DE NODOS Y QUE CADA NODO TENGA SU LISTA DE ARCOS.
+struct NodoGrafo{
+    int id;
+    struct NodoArco* listaArcos;
+    struct NodoGrafo* siguiente;
+};
+struct NodoArco{
+    int id;
+    struct NodoGrafo* destino;
+    struct NodoArco* siguiente;
+};
 
 int main() {
     
