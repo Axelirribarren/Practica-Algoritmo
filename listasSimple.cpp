20ab06b189f5e0d7e0be57a92c1a2d3055643677
Axelirribarren <axelirribarren@gmail.com>
2025-10-08 20:22:23 -0300

    Agregue funciones con mostrar y liberar

--- listasSimple.cpp
+++ listasSimple.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 struct Nodo{
     int dato;
-    Nodo * link;
+    Nodo*link;
 };
 
-void agregarNodo(Nodo*nodo, int valor){
+void insertarNodo(Nodo*&nodo, int valor){
     Nodo*nuevo = new Nodo;
     nuevo -> dato = valor;
     nuevo -> link = nodo;
     nodo = nuevo;
 }
-void agregarFinalNodo(Nodo*nodo, int valor){
+void insertarNodoFinal(Nodo*&nodo, int valor){
     Nodo*nuevo = new Nodo;
     nuevo -> dato = valor;
     nuevo -> link = NULL;
@@ -21,11 +21,39 @@ void agregarFinalNodo(Nodo*nodo, int valor){
         nodo = nuevo;
         return;
     }
-    Nodo * aux = nodo;
+    Nodo*aux = nodo;
     while(aux -> link != NULL) aux = aux -> link;
     aux -> link = nuevo;
 }
+
+void mostrar(Nodo*&nodo){
+    Nodo * aux = nodo;
+    cout << "LISTA: ";
+    while(aux != NULL){
+        cout << aux -> dato << " -> ";
+        aux = aux -> link;
+    }
+    cout << "NULL" << endl;
+}
+
+void liberar(Nodo*&nodo){
+    Nodo*aux;
+    while(nodo != NULL){
+        aux = nodo;
+        nodo = nodo -> link;
+        delete aux;
+    }
+}
 int main() {
-    
+    Nodo*prueba = NULL;
+    insertarNodo(prueba, 1);
+    insertarNodo(prueba, 2);
+    insertarNodo(prueba, 3);
+    insertarNodoFinal(prueba, 1);
+    mostrar(prueba);
+    liberar(prueba);
+
+    system("pause");
+
     return 0;
 }
