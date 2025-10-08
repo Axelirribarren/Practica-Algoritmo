3b2594e420284e387b5b42759a8626422b479112
Axelirribarren <axelirribarren@gmail.com>
2025-10-07 23:56:02 -0300

    probando nodos y arcos

--- practicamia.cpp
+++ practicamia.cpp
@@ -13,10 +13,7 @@ struct NodoArco{
     struct NodoArco* siguiente;
 };
 
-int main() {
-    
-    return 0;
-}
+
 //Crear el nuevo "nodo" para el grafo
 NodoGrafo * crearNodoGrafo(int id){
     NodoGrafo* nuevoNodo = new NodoGrafo;
@@ -30,4 +27,45 @@ void agregarNodo(NodoGrafo*&grafo, int id){
     NodoGrafo*nuevo = crearNodoGrafo(id);
     nuevo -> siguiente = grafo;
     grafo = nuevo;
+}
+NodoArco* crearNodoArco(int id, NodoGrafo*destino){
+    NodoArco*nuevoArco = new NodoArco;
+    nuevoArco -> id = id;
+    nuevoArco -> destino = destino;
+    nuevoArco ->siguiente = nullptr;
+    return nuevoArco;
+}
+void agregarArco(NodoGrafo*origen, NodoGrafo*destino,int id){
+    if(origen!=nullptr && destino!=nullptr){
+        NodoArco*nuevoArco = crearNodoArco(id,destino);
+        nuevoArco -> siguiente = origen->listaArcos;
+        origen-> listaArcos = nuevoArco;
+    }
+    
+}
+void mostrarListaAdyacencia(NodoGrafo*grafo){
+    NodoGrafo*actual = grafo;
+    while(actual != nullptr){
+        cout << "nodo " << actual -> id << " -> ";
+        NodoArco*arco = actual -> listaArcos;
+        while(arco!=nullptr){
+            cout << arco-> id << " ";
+            arco = arco -> siguiente;
+        }
+        cout << endl;
+        actual = actual -> siguiente;
+    }
+}
+int main() {
+    NodoGrafo*grafo = nullptr;
+    agregarNodo(grafo, 1);
+    agregarNodo(grafo, 2);
+    agregarNodo(grafo, 3);
+    agregarArco(grafo,grafo->siguiente,2);
+    agregarArco(grafo,grafo->siguiente->siguiente,3);
+    agregarArco(grafo->siguiente,grafo->siguiente->siguiente,3);
+    
+    mostrarListaAdyacencia(grafo);
+    system("pause");
+    return 0;
 }
